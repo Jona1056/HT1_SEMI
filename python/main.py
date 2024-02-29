b2ed6ff33b7e3a4faad7d866823219a1a2dd1571
Jona1056 <3685713940101@ingenieria.usac.edu.gt>
2024-02-28 21:59:45 -0600

    arreglo

--- python/main.py
+++ python/main.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'OK', 200
-
-@app.route('/check')
-def check():
     data = {
         "Instancia": "Instancia #1 - API #1",
         "Curso": "Seminario de Sistemas 1",
         "Estudiante": "Jonatan Garcia - 202000424"
     }
     return jsonify(data)
+
+@app.route('/check')
+def check():
+   return 'OK', 200
     
 
 
